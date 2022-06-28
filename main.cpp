29acfabec4f5f40988c67728fb5408b2f25291e1
Charles Goedefroit <charles.goedefroit@inria.fr>
2022-06-28 09:56:39 +0200

    add cast test

--- main.cpp
+++ main.cpp
@@ -3,6 +3,22 @@
 #include <limits.h>
 #include <iostream>
 
+void printBytes(u_char* bytes, size_t nbBytes) {
+  printf("{ ");
+  for (size_t nbByte = 0; nbByte < nbBytes; nbByte++) {
+    for (int i = 7; i >= 0; i--)
+      printf((bytes[nbByte] & (1 << i)) ? "1" : "0");
+    printf(" ");
+  }
+  printf("}\n");
+}
+
+template <typename T>
+void printVar(const T& var) {
+  // std::cout << var << " = ";
+  printBytes((u_char*)&var, sizeof(T));
+}
+
 int main(int argc, char const *argv[])
 {
   printf("int : %ld\n", sizeof(int));
@@ -60,12 +76,83 @@ int main(int argc, char const *argv[])
   printf("test for %%n in this sen%ntence\n", &len);
   printf("%d\n", len);
 
+  {
+    printf("\nCast float to int\n");
+
+    float f = 27.53;
+    printf("%-24s %-8.2f = ", "origin value :", f);
+    printVar(f);
+
+    int fi = (int)f;
+    printf("%-24s %-8d = ", "int C-style cast :", fi);
+    printVar(fi);
+    int fisc = static_cast<int>(f);
+    printf("%-24s %-8d = ", "int static cast :", fisc);
+    printVar(fisc);
+
+    int fip = *(int*)&f;
+    printf("%-24s %-8d = ", "int pointer cast :", fip);
+    printVar(fip);
+
+    int fircp = *reinterpret_cast<int*>(&f);
+    printf("%-24s %-8d = ", "int reinterpret cast :", fircp);
+    printVar(fircp);
+
+    const float cf = 27.53;
+    printf("%-24s %-8.2f = ", "origine const value :", cf);
+    printVar(cf);
+    float* ficc = const_cast<float*>(&cf);
+    *ficc = 27.02;
+    printf("%-24s %-8.2f = ", "edit const cast value :", *ficc);
+    printVar(cf);
+
+  }
+
+  {
+    printf("\nCast uint16_t to uint8_t\n");
+
+    short s = 258;
+    printf("%-24s %-8hu = ", "origin value :", s);
+    printVar(s);
+
+    int8_t sc = (int8_t)s;
+    printf("%-24s %-8hhu = ", "uint8 C-style cast :", sc);
+    printVar(sc);
+    int8_t scsc = static_cast<int8_t>(s);
+    printf("%-24s %-8hhu = ", "uint8 static cast :", scsc);
+    printVar(scsc);
+
+    int8_t sccp = *(int8_t *)&s;
+    printf("%-24s %-8hhu = ", "uint8 pointer cast :", sccp);
+    printVar(sccp);
+
+    int8_t scrcp = *reinterpret_cast<int8_t*>(&s);
+    printf("%-24s %-8hhu = ", "uint8 reinterpret cast :", scrcp);
+    printVar(scrcp);
+  }
+
+    {
+    printf("\nCast uint8_t to uint16_t\n");
+
+    int8_t c = 2;
+    printf("%-25s %-8hhu = ", "origin value :", c);
+    printVar(c);
+
+    short cc = (short)c;
+    printf("%-25s %-8hu = ", "uint16 C-style cast :", cc);
+    printVar(cc);
+    short ccsc = static_cast<short>(c);
+    printf("%-25s %-8hu = ", "uint16 static cast :", ccsc);
+    printVar(ccsc);
+
+    short cccp = *(short *)&c;
+    printf("%-25s %-8hu = ", "uint16 pointer cast :", cccp);
+    printVar(cccp);
+
+    short ccrcp = *reinterpret_cast<short*>(&c);
+    printf("%-25s %-8hu = ", "uint16 reinterpret cast :", ccrcp);
+    printVar(ccrcp);
+  }
+
   return 0;
 }
-
-
-
-
-
-
-
